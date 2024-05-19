d66f86c87da7823968bf4774560dc51c31e4f731
Riekus <riekusritskes@gmail.com>
2024-05-19 16:28:50 +0200

    moved api version to connection, fixed input for Query node, simplified API dropdown selection

--- src/salesforce-query.js
+++ src/salesforce-query.js
@@ -6,30 +6,30 @@ module.exports = function (RED) {
   function SalesforceQueryNode(config) {
     RED.nodes.createNode(this, config);
     this.active = true;
-    this.connection = config.connection;
+    this.connection = RED.nodes.getCredentials(this.connection);
+
     this.soql = config.soql || '';
 
-    const connectionConfig = RED.nodes.getNode(this.connection);
-    const credentials = RED.nodes.getCredentials(this.connection);
 
-    if (connectionConfig && connectionConfig.oauth) {
-      this.log(connectionConfig);
-      this.log(credentials);
-    } else {
-      this.error(RED._('salesforce.errors.missingcredentials'));
-    }
+    // if (connectionConfig && connectionConfig.oauth) {
+    //   this.log(connectionConfig);
+    //   this.log(credentials);
+    // } else {
+    //   this.error(RED._('salesforce.errors.missingcredentials'));
+    // }
+    let node = this;
+    this.on('input', function(msg, send, done) {
+
+        msg.payload = "succes";
+        send(msg);
+    
+
+        done();
+    });
+
+
+
   }
-//   let node = this; 
-//   this.on('input', function(msg, send, done) {
-    
-//     console.log(msg);
-
-//     msg.payload = 'success'; 
-    
-//     send(msg); 
-
-//     done();
-// });
 
   RED.nodes.registerType('salesforce-query', SalesforceQueryNode);
 };
