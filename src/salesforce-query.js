a1fdc55a1f7d942cb594890de7e3ad5b73d02ed6
Riekus <riekusritskes@gmail.com>
2024-05-19 19:21:45 +0200

    added comment about handling connection

--- src/salesforce-query.js
+++ src/salesforce-query.js
@@ -6,7 +6,11 @@ module.exports = function (RED) {
   function SalesforceQueryNode(config) {
     RED.nodes.createNode(this, config);
     this.active = true;
+
+    // Not sure how to do this propperly? think easiest will be to have authenticated connection in here?
+    // So handling the authentication / re-authentication in the connection node? No sure what the best ways is. 
     this.connection = RED.nodes.getCredentials(this.connection);
+    this.connection = RED.nodes.getNode(config.connection);
 
     this.soql = config.soql || '';
 
